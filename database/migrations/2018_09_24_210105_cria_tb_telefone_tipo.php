e24c77f77d7cd76d98884b18ad356c2564269f34
carloshans1 <carlosoliveira@cobra002079.saovicente.local>
2018-09-24 18:07:33 -0300

    teste 2

---
+++ database/migrations/2018_09_24_210105_cria_tb_telefone_tipo.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CriaTbTelefoneTipo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('telefone_tipo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tipo', 50);
+            $table->integer('telefone_tipo')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('telefone_tipo');
+    }
+}
